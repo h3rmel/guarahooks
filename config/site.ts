31335347918bed85b173994f832c85a1ec06842c
h3rmel <isaach3rmel@gmail.com>
2025-10-08 13:01:25 -0300

    Revert "starting reboot..."
    This reverts commit adcbbd338890717ae7bcb52164a959c9ffef54e0.

---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = {
+  name: 'guarahooks',
+  url: 'https://guarahooks.com',
+  description:
+    'A free, open-source collection of reusable React hooks you can copy and paste into your apps.',
+  links: {
+    github: 'https://github.com/h3rmel/guarahooks',
+  },
+  authorUrl: 'https://github.com/h3rmel',
+  keywords: [
+    'React',
+    'Next.js',
+    'Hooks',
+    'TypeScript',
+    'Type-safe',
+    'Hooks Library',
+  ],
+  ogImage: 'https://guarahooks.com/og.webp',
+};
+
+export type SiteConfig = typeof siteConfig;
